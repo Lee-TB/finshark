f55c413552bab34ddd1fcf2f7dc10195a4a6c98a
Tommy Tran <tblyduc2412@gmail.com>
2024-04-04 16:02:17 +0700

    Login

--- api/Controllers/AccountController.cs
+++ api/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using api.Interfaces;
 using api.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Controllers;
 
@@ -12,13 +13,45 @@ public class AccountController : ControllerBase
 {
     private readonly UserManager<AppUser> _userManager;
     private readonly ITokenService _tokenService;
-    public AccountController(UserManager<AppUser> userManager, ITokenService tokenService)
+    private readonly SignInManager<AppUser> _signinManager;
+    public AccountController(UserManager<AppUser> userManager, ITokenService tokenService, SignInManager<AppUser> signInManager)
     {
         _userManager = userManager;
         _tokenService = tokenService;
+        _signinManager = signInManager;
     }
 
-    [HttpPost("Register")]
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(LoginDto loginDto)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var user = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
+
+        if (user == null)
+        {
+            return Unauthorized("Invalid username");
+        }
+
+        var result = await _signinManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+        if (!result.Succeeded)
+        {
+            return Unauthorized("Password incorrect");
+        }
+
+        return Ok(new NewUserDto
+        {
+            Username = user.UserName,
+            Email = user.Email,
+            Token = _tokenService.CreateToken(user),
+        });
+    }
+
+    [HttpPost("register")]
     public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
     {
         try
