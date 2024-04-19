c8376c8c201d1eca9b8c57064f8dbfd96daf7136
Tommy Tran <tblyduc2412@gmail.com>
2024-04-19 16:46:38 +0700

    TenK Finder

---
+++ frontend/src/Components/TenKFinder/TenKFinder.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import { CompanyTenK } from "../../company";
+import { getTenK } from "../../api";
+import TenKFinderItem from "./TenKFinderItem/TenKFinderItem";
+import Spinner from "../Spinner/Spinner";
+
+type Props = {
+  ticker: string;
+};
+
+const TenKFinder = ({ ticker }: Props) => {
+  const [companyData, setCompanyData] = useState<CompanyTenK[]>();
+
+  useEffect(() => {
+    const getTenKData = async () => {
+      const result = await getTenK(ticker);
+      setCompanyData(result?.data);
+    };
+    getTenKData();
+  }, []);
+
+  return (
+    <div className="inline-flex rounded-md shadow-sm m-4 gap-2">
+      {companyData ? (
+        companyData?.slice(0, 5).map((tenK) => <TenKFinderItem tenK={tenK} />)
+      ) : (
+        <Spinner />
+      )}
+    </div>
+  );
+};
+
+export default TenKFinder;
--- frontend/src/Pages/CompanyPage/CompanyPage.tsx
+++ frontend/src/Pages/CompanyPage/CompanyPage.tsx
@@ -6,6 +6,7 @@ import Sidebar from "../../Components/Sidebar/Sidebar";
 import CompanyDashboard from "../../Components/CompanyDashboard/CompanyDashboard";
 import Tile from "../../Components/Tile/Tile";
 import Spinner from "../../Components/Spinner/Spinner";
+import TenKFinder from "../../Components/TenKFinder/TenKFinder";
 
 interface Props {}
 
@@ -34,6 +35,8 @@ const CompanyPage = (props: Props) => {
             <Tile title="Price" subtitle={company.price.toString()} />
             <Tile title="Sector" subtitle={company.sector} />
             <Tile title="DCF" subtitle={company.dcf.toString()} />
+            <TenKFinder ticker={ticker!} />
+
             <p className="bg-white shadow rounded text-medium text-gray-900 p-3 mt-1 m-4">
               {company.description}
             </p>
